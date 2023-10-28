6d66295edade92231a2254680a4268d34eec592d
Nileena2903 <nileenakr333@gmail.com>
2023-10-28 10:21:36 +0530

    initialcommit

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MaterialApp(home: Center(child: Text('Hello',style: TextStyle(color: Colors.blueGrey,decoration: TextDecoration.none )),),));
+}
+
